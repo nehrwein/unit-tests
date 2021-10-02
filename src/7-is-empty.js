ffe722e5b31e42b1de4a27a85ca54f7bbd764dba
Birgit Nehrwein <nehrwein@posteo.de>
2021-10-02 18:37:25 +0200

    fixed bugs on 1-7

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -3,7 +3,9 @@ export const isEmpty = (stringArrayOrObject) => {
 
   if (type === 'string') {
     return stringArrayOrObject === ''
-  }
+  } else if (type === 'object' && stringArrayOrObject.length === 0 || Object.keys(stringArrayOrObject).length === 0) {
+    return true
+  } 
 
   return false
 }
--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,4 @@
 export const hackerSpeak = (words) => {
+  words = words.replace('a','e','i','o','s').replace('4','3','1','0','5')
   return words
 }
